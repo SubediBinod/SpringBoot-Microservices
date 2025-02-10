fa59b02292c070ebfd02426806da2a8fd40b8e28
Binod <bidhaksubedi77@gmail.com>
2025-02-10 20:18:34 +0530

    Company Microservices

--- JobMicroservices/src/main/java/com/binod/JobMicroservices/Job/repo/JobRepo.java
+++
@@ -1,9 +0,0 @@
-package com.binod.Project1.Job.repo;
-
-import com.binod.Project1.Job.entity.Job;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JobRepo extends JpaRepository<Job,Integer> {
-}
